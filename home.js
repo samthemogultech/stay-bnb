18ddbdf3630bbe602849bbe1731f5aabe533cc43
samthemogultech <samthemogul.tech@gmail.com>
2023-09-02 13:40:40 +0100

    first bnb

---
+++ home.js
@@ -0,0 +1,6 @@
+let navBar = document.getElementById("navBar");
+//let activemenu = document.getElementsByClassName("menu-items");
+
+function togglebtn(){
+    navBar.classList.toggle("hidemenu")
+}
